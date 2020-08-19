4ed679ba271d97ca12d6c831adc55a01a66659f7
Bhavya Mallick <bhavyamallick99@gmail.com>
2020-08-20 01:25:07 +0530

    App.js created

---
+++ srv/routes/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5009;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    createIndexes: true,
+  })
+  .catch(function () {
+    console.log("DB connection error");
+  });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log(`MongoDB database connection established successfully`);
+});
+
+const walletRouter = require("./routes/wallet");
+
+app.use("/wallets", walletRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
